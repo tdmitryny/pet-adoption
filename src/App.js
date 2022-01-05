a8ebaa3f844b70d28916fbbc0af909103439277b
tdmitryny <tdmitryny@gmail.com>
2022-01-05 14:17:43 -0500

    Change all status

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Navigation from './components/navigation';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Navigation />
+        <Route path="/:type/:id">
+          <PetDetailsPage />
+        </Route>
+        <Route path="/:type?">
+          <HomePage />
+        </Route>
+        {/* <Route path="/home/:title?">
+          <HomePage />
+        </Route> */}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
